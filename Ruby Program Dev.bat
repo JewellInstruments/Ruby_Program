4f3147590d264ea00742329e01db0edc634de0c9
cbrochu1 <cbrochu@jewellinstruments.com>
2025-07-29 14:04:25 -0400

    fix .bat

--- Ruby Program Dev.bat
+++ Ruby Program Dev.bat
@@ -1,4 +1,4 @@
-@REM git pull https://github.com/JewellInstruments/Ruby_Program.git main
+git pull https://github.com/JewellInstruments/Ruby_Program.git main
 call ".venv\Scripts\activate.bat"
 @REM pip install -U -r requirements.txt
 python main.py
