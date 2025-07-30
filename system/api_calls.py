ab19659d88f979e7c2b087b95279fcf706513e0a
cbrochu1 <cbrochu@jewellinstruments.com>
2025-07-30 08:59:40 -0400

    update

--- system/api_calls.py
+++ system/api_calls.py
@@ -877,9 +877,18 @@ def get_RUBY_label_current_number(week_of_the_year, number_of_units, part_number
     data = {"", "879838", "I0PAI-0000"}
     data2 = {"serial_number": "0", "part_number": "879838", "work_order": "I0PAI-0000"}
     data3 = list(data)
-    table_data = api_handler.post("serial_number/", data)
-    table_data = api_handler.post("serial_number/", data2)
-    table_data = api_handler.post("serial_number/", data3)
+    try:
+        table_data = api_handler.post("serial_number/", data)
+    except:
+        print("data did not work")
+    try:
+        table_data = api_handler.post("serial_number/", data2)
+    except:
+        print("data2 did not work")
+    try:
+        table_data = api_handler.post("serial_number/", data3)
+    except:
+        print("data3 did not work")
     print(f"table_1: {table_data}")
 
     table_data = table_data.data
